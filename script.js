161f88eee865ed538b2fc770d04c4734990ddec0
espiacent <ch.gripp@posteo.ch>
2022-08-10 19:36:56 +0200

    add canvas colour switch

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ dropdown1.addEventListener('change', (event) => {
     const mult = event.target.value * event.target.value;
     canvas.style.gridTemplateColumns = `repeat(${event.target.value}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${event.target.value}, 1fr)`;
-    console.log(canvas.style);
     canvas.innerHTML = "";
     for (let i = 1; i < (mult + 1); i++) {
         canvas.innerHTML += `<div class="gridfield" id="${i}"</div>`;
@@ -42,7 +41,16 @@ dropdown1.addEventListener('change', (event) => {
 });
 
 dropdown2.addEventListener('change', (event) => {
-    console.log(`${event.target.value}`);
+    const canvas = document.querySelectorAll('.gridfield');
+    canvas.forEach(field => {
+        if (event.target.value == 'black') {
+            field.style.backgroundColor = `${event.target.value}`;
+            field.style.borderColor = 'grey';
+        } else {
+            field.style.backgroundColor = `${event.target.value}`;
+            field.style.borderColor = 'black';
+        }
+    });
 });
 
 dropdown3.addEventListener('change', (event) => {
@@ -50,12 +58,8 @@ dropdown3.addEventListener('change', (event) => {
 });
 
 button1.addEventListener('click', (event) => {
-    const canvas = document.querySelectorAll('.gridfield')
+    const canvas = document.querySelectorAll('.gridfield');
     canvas.forEach(field => {
         field.style.backgroundColor = "rgb(223, 223, 223)";
     });
 });
-
-// grid-template-columns: repeat(16, 1fr);
-// grid-template-rows: repeat(16, 1fr);
-
