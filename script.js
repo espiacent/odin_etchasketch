dee946fb18cd85dea9e6500b8887ff202bb84f4d
espiacent <ch.gripp@posteo.ch>
2022-08-10 10:43:54 +0200

    add clear button

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const paintboard = document.querySelector('.paintboard');
+const canvas = document.querySelector('.canvas');
 
 for (let i = 1; i < 257; i++) {
-    paintboard.innerHTML += `<div class="gridfield" id="${i}"</div>`;
+    canvas.innerHTML += `<div class="gridfield" id="${i}"</div>`;
 };
 
-const gridfields = paintboard.querySelectorAll('.gridfield');
+const gridfields = canvas.querySelectorAll('.gridfield');
 
 let mouseDown = false
 window.onmousedown = () => (mouseDown = true);
 window.onmouseup = () => (mouseDown = false);
 
-paintboard.addEventListener('mouseover', e => {
+canvas.addEventListener('mouseover', e => {
     if (mouseDown) {
         const position = (document.elementFromPoint(e.clientX, e.clientY));
         const gridId = position.id;
         document.getElementById(`${gridId}`).style.backgroundColor = "black";
-        console.log(gridId);
     }
 }, { passive: true })
+
+canvas.addEventListener('click', e => {
+    const position = (document.elementFromPoint(e.clientX, e.clientY));
+    const gridId = position.id;
+    document.getElementById(`${gridId}`).style.backgroundColor = "black";
+}, { passive: true })
+
+// const btn1 = document.querySelector('.size');
+// const btn2 = document.querySelector('.canvascolor');
+// const btn3 = document.querySelector('.pencolor');
+const btn4 = document.querySelector('.clear');
+
+// btn1.addEventListener('click', rock);
+// btn2.addEventListener('click', paper);
+// btn3.addEventListener('click', scissors);
+btn4.addEventListener('click', clearCanvas);
+
+function clearCanvas() {
+    const canvas = document.querySelectorAll('.gridfield')
+    canvas.forEach(field => {
+        field.style.backgroundColor = "rgb(223, 223, 223)";
+    });
+}
